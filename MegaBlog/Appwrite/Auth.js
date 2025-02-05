bc4a8f9611301a1642047c8a7af8498878859acb
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-05 11:06:25 +0530

    Authentication

---
+++ MegaBlog/Appwrite/Auth.js
@@ -0,0 +1,55 @@
+import conf from "../src/conf/conf";
+import {Client,Account,ID} from "appwrite";
+ export class AuthService{
+    client =new Client();
+    account ;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account=new Account(this.client); 
+    }
+    async createAccount({email,password,name}){
+        try{
+            const userAccount=await this.account.create(ID.unique(),email,password,name)
+            if(userAccount){
+                return this.login({email,password});
+            }
+            else{
+                throw new Error("Account not created"); 
+            }
+
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async getUser(){
+        try{
+            return this.account.get()
+        }
+        catch(error){
+            throw error
+        }
+        return null;
+    }
+    async logout(){
+        try{
+            await this.account.deleteSessions()
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+ }
+ const authService=new AuthService();
+ export default authService;
