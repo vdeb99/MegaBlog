0035744ae0d578cd43490f9d674109d38e35e17f
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-14 18:36:16 +0530

    Logo component

---
+++ MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Logo({width='100px'}){
+    return (
+        <div className="text-red-600">
+            <h1>Logo</h1>
+        </div>
+    );
+}
