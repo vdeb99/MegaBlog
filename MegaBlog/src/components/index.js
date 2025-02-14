7c0f840e8de857e7f15454a741853cc7fdc3952c
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-14 18:35:06 +0530

    exportion of all the components files

---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import PostForm from "./postForm/PostForm";
+import Signup from "./Signup";
+import Login from "./Login";
+import Protected from "./AuthLayout";
+import PostCard from "./PostCard";
+export{Header,Footer,Container,Logo,LogoutBtn,RTE,Button,Input,Select,PostForm,Signup,Login,Protected,PostCard};
