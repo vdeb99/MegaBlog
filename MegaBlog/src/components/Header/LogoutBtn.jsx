9e823cfb05abba8811c775539cef6724938f9fd0
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-14 18:31:00 +0530

    containing Home/All-Posts/(Signup/Logout) pages routes

---
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../../Appwrite/Auth";
+import { logout } from "../../Store/AuthSlice";
+
+export default function LogoutBtn() {
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        authService.logout()
+            .then(() => {
+                dispatch(logout()); 
+            })
+            .catch((error) => {
+                console.error("Logout Error:", error);
+            });
+    };
+
+    return (
+        <button 
+            onClick={logoutHandler}  
+            className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 text-white rounded-full"
+        >
+            Logout
+        </button>
+    );
+}
