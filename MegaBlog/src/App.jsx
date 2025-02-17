7ad00f73d7c0820f605176364f46915fecfea957
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-17 19:21:43 +0530

    Base connection

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import authService from "../Appwrite/Auth"
+import {login, logout} from "./Store/AuthSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>Blog with Appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className="flex flex-col min-h-screen">
+      
+      {/* Sticky Header */}
+      <header className="sticky top-0 z-50 shadow-md">
+        <Header />
+      </header>
+
+      {/* Main Content (pushes footer down) */}
+      <main className="flex-1 bg-gray-500">
+        <Outlet />
+      </main>
+
+      {/* Footer at bottom */}
+      <Footer />
+    </div>
+  ) : null;
 }
 
 export default App
--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,10 +1,94 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './Store/Store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import { Protected, Login } from './components/index.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+import AddPost from "./Pages/AddPost";
+import Signup from './components/Signup';
+import EditPosts from "./Pages/EditPosts";
+
+import Post from "./Pages/Post";
+
+import AllPosts from "./Pages/AllPosts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            key: "home",
+            path: "/",
+            element: <Home />,
+        },
+        {
+            key: "login",
+            path: "/login",
+            element: (
+                
+                <Protected authentication={false}>
+                    <Login />
+                </Protected>
+            ),
+        },
+        {
+            key: "signup",
+            path: "/signup",
+            element: (
+                <Protected authentication={false}>
+                    <Signup />
+                </Protected>
+            ),
+        },
+        {
+            key: "all-posts",
+            path: "/all-posts",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <AllPosts />
+                </Protected>
+            ),
+        },
+        {
+            key: "add-post",
+            path: "/add-post",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <AddPost />
+                </Protected>
+            ),
+        },
+        {
+            key: "edit-post",
+            path: "/edit-post/:slug",
+            element: (
+                <Protected authentication>
+                    {" "}
+                    <EditPosts />
+                </Protected>
+            ),
+        },
+        {
+            key: "post",
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>,
 )
