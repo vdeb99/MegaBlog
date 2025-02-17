368a0664fd789ff63a3038bbb883b767a12bb554
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-17 19:16:23 +0530

    Signup page

---
+++ MegaBlog/src/Pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Signup as SignupComponent } from "../components/index";
+export default function Signup(){
+    return(
+        <div className="">
+            <SignupComponent />
+        </div>
+    )
+}
