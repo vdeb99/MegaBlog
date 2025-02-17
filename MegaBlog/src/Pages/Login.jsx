046f42876dea0fb281f601b483aeb0335f067a34
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-17 19:16:05 +0530

    Login page

---
+++ MegaBlog/src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Login as LoginComponent} from "../components";
+export default function Login(){
+    return(
+        <div className="">
+            <LoginComponent />
+        </div>
+    )
+}
